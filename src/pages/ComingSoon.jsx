08ee6ff4f93d4e92289c8d1f0cf6efccff0b36fa
the-geniustech <holumidey70@gmail.com>
2024-08-28 22:05:59 +0100

    Coming Soon Page...

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -58,7 +58,7 @@ function ComingSoon() {
         <div className="flex flex-col justify-end items-center gap-10 md:gap-16 lg:gap-28 bg-black/75 px-10 md:px-16 pt-10 md:pt-16 pb-10 rounded-2xl w-full">
           <div className="flex flex-col justify-end items-center">
             {/* <img src="/icon/logo40.svg" alt="pagedone logo" /> */}
-            <div className="-ml-6 w-60">
+            <div className="-ml-6 w-48 sm:w-60">
               <LogoWhite />
             </div>
             <div className="flex flex-col justify-center items-center gap-4 sm:gap-6 md:gap-10">
@@ -66,7 +66,7 @@ function ComingSoon() {
                 <h2 className="shadow-sm font-bold text-4xl text-center text-emerald-600 sm:text-5xl md:text-6xl leading-normal">
                   Coming Soon
                 </h2>
-                <p className="text-base text-center text-gray-200 leading-relaxed">
+                <p className="text-center text-gray-200 sm:text-base leading-relaxed">
                   Just 20 days remaining until the big reveal of our new
                   product!
                 </p>
@@ -88,13 +88,13 @@ function ComingSoon() {
                 ))}
               </div>
               <div className="flex flex-col justify-center items-center gap-5 w-full">
-                <h6 className="font-semibold text-base text-center text-white leading-relaxed">
+                {/* <h6 className="font-semibold text-base text-center text-white leading-relaxed">
                   Launched Date: July 23, 2024
-                </h6>
-                <div className="flex sm:flex-row flex-col justify-center items-center gap-2.5">
+                </h6> */}
+                <form className="flex sm:flex-row flex-col justify-center items-center gap-2.5">
                   <input
                     type="text"
-                    className="w-80 focus:outline-none border-gray-200 bg-white shadow-sm px-3.5 py-2 border rounded-lg h-10 text-gray-900 text-sm leading-relaxed placeholder-gray-400"
+                    className="border-gray-200 bg-white shadow-sm px-3.5 py-2 border rounded-lg w-full max-w-80 focus:outline-none h-10 text-gray-900 text-sm leading-relaxed placeholder-gray-400"
                     placeholder="Type your mail..."
                   />
                   <button className="flex justify-center items-center bg-emerald-500 hover:bg-emerald-600 shadow-sm px-3.5 py-2 rounded-lg w-full sm:w-fit transition-all duration-700 ease-in-out">
@@ -102,17 +102,17 @@ function ComingSoon() {
                       Notify Me
                     </span>
                   </button>
-                </div>
+                </form>
               </div>
             </div>
           </div>
-          <p className="text-center text-gray-200 text-sm leading-snug">
+          <p className="text-base text-center text-gray-200 sm:text-sm leading-snug">
             Get in touch with us:{" "}
             <a
               href="mailto:mail@pagedone.com"
               className="hover:text-gray-100 transition-all duration-700 ease-in-out"
             >
-              mail@pagedone.com
+              trypayertel@gmail.com
             </a>
           </p>
         </div>
